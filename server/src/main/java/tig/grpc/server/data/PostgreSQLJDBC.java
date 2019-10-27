fc226fd92bc5ec51bbdfa376aa08fb2f0b27c860
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2019-10-27 18:25:43 +0000

    Database connection

--- server/src/main/java/tig/grpc/server/data/PostgreSQLJDBC.java
+++ server/src/main/java/tig/grpc/server/data/PostgreSQLJDBC.java
@@ -1,4 +1,22 @@
 package tig.grpc.server.data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class PostgreSQLJDBC {
+
+    public PostgreSQLJDBC(int port, String password) {
+        Connection c = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:" + Integer.toString(port) + "/tigdb",
+                            "postgres", password);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
 }
--- server/src/main/java/tig/grpc/server/service/DocumentServer.java
+++ server/src/main/java/tig/grpc/server/service/DocumentServer.java
@@ -4,28 +4,24 @@ import io.grpc.Server;
 import io.grpc.BindableService;
 import io.grpc.ServerBuilder;
 import tig.grpc.server.api.TigServiceImpl;
+import tig.grpc.server.data.PostgreSQLJDBC;
 
 public class DocumentServer {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println(DocumentServer.class.getSimpleName());
 
-		// receive and print arguments
-		System.out.printf("Received %d arguments%n", args.length);
-		for (int i = 0; i < args.length; i++) {
-			System.out.printf("arg[%d] = %s%n", i, args[i]);
-		}
-
 		// check arguments
 		if (args.length < 1) {
 			System.err.println("Argument(s) missing!");
-			System.err.printf("Usage: java %s port%n", DocumentServer.class.getName());
+			System.err.printf("<Usage> java %s port%n", DocumentServer.class.getName());
 			return;
 		}
 
 		final int port = Integer.parseInt(args[0]);
 		final BindableService impl = new TigServiceImpl();
 
+		PostgreSQLJDBC db = new PostgreSQLJDBC(5432, "root");
 		// Create a new server to listen on port
 		Server server = ServerBuilder.forPort(port).addService(impl).build();
 
