db7964708b23cc177d4ab7c3c0f22d4454cfd4d7
Carolina Carreira <carolina.carreira@tecnico.ulisboa.pt>
2019-11-01 18:29:33 +0000

    added Deletefile to usersDAO

--- server/src/main/java/tig/grpc/server/data/dao/UsersDAO.java
+++ server/src/main/java/tig/grpc/server/data/dao/UsersDAO.java
@@ -69,5 +69,33 @@ public class UsersDAO {
             throw new RuntimeException();
         }
     }
+
+    public static void deleteFile(String username, String filename) {
+
+        Connection conn = PostgreSQLJDBC.getInstance().getConn();
+        try {
+            // verificar se user tem esse file
+            PreparedStatement stmt = conn.prepareStatement("SELECT fileid FROM authorizations WHERE username = (?) and filename = (?)");
+            stmt.setString(1, username);
+            stmt.setString(2, filename);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                // apagar o file
+                PreparedStatement delete_stmt = conn.prepareStatement("DELETE FROM files WHERE fileid = (?)");
+                delete_stmt.setString(1, rs.getString("fileid"));
+                delete_stmt.executeUpdate();
+            } else
+                throw new IllegalArgumentException("No such file name.");
+
+            /* Tambem posso fazer query toda junta
+            PreparedStatement stmt = conn.prepareStatement("DELETE FROM files UNION authorizations ON files.fileid = authorizations.fileid WHERE username = (?) and filename = (?)");
+            mas assim nao faz verificacao segue
+             */
+        } catch (SQLException e) {
+            // TODO rever
+            throw new IllegalArgumentException("No such file name.");
+        }
+
+    }
 }
 
