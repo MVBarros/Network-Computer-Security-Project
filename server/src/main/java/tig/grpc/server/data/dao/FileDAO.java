3d42a2a9461b078b9163fc2c58a71bcee929b6c3
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2019-11-06 19:53:56 +0000

    ListFiles fix

--- server/src/main/java/tig/grpc/server/data/dao/FileDAO.java
+++ server/src/main/java/tig/grpc/server/data/dao/FileDAO.java
@@ -1,6 +1,5 @@
 package tig.grpc.server.data.dao;
 
-import com.google.protobuf.ByteString;
 import tig.grpc.server.data.PostgreSQLJDBC;
 import tig.grpc.server.utils.EncryptionUtils;
 import tig.grpc.server.utils.StringGenerator;
@@ -12,27 +11,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class FileDAO {
-
-    /*public static String getFilename(String fileID) {
-        Connection conn = PostgreSQLJDBC.getInstance().getConn();
-
-        try {
-            PreparedStatement stmt = conn.prepareStatement("SELECT filename FROM files WHERE fileId =(?)");
-            stmt.setString(1, fileID);
-
-
-            ResultSet rs = stmt.executeQuery();
-            rs.next();
-            return rs.getString("filename");
-
-        } catch (SQLException e) {
-            //Should never happen
-            throw new RuntimeException();
-        }
-    }*/
 
     public static void fileUpload(String filename, byte[] fileContent, String username) {
         Connection conn = PostgreSQLJDBC.getInstance().getConn();
@@ -56,7 +38,6 @@ public class FileDAO {
             //Primary Key violation
             throw new IllegalArgumentException("Filename Provided already exists");
         }
-        AuthenticationDAO.createAuth(filename, username, username, 1);
     }
 
     public static void fileEdit(String filename, byte[] fileContent, String owner) {
@@ -115,36 +96,35 @@ public class FileDAO {
 
     }
 
-    public static  List<String> listFiles(String username) {
+    public static List<String> listFiles(String username) {
         Connection conn = PostgreSQLJDBC.getInstance().getConn();
 
         PreparedStatement stmt = null;
         try {
+            HashSet<String> ownedFiles = new HashSet<String>();
             // TODO rever
-            stmt = conn.prepareStatement("SELECT filename,owner FROM files WHERE owner = (?)");
+            stmt = conn.prepareStatement("SELECT username,permission, filename, owner FROM files NATURAL JOIN authorizations WHERE owner = (?) or user = (?)");
             stmt.setString(1, username);
+            stmt.setString(2, username);
             ResultSet rs = stmt.executeQuery();
 
             List<String> result = new ArrayList<String>();
             while (rs.next()) {
-                result.add(rs.getString("filename") + " " + rs.getString("owner"));
-            }
+                if (rs.getString("username") == rs.getString("owner")) {
+                    if (!ownedFiles.contains(rs.getString("filename"))) {
+                        result.add(String.format("File: %s Owner: %s Permission: R/W", rs.getString("filename"), rs.getString("owner")));
+                        ownedFiles.add(rs.getString("filename"));
+                    }
+                } else {
+                    result.add(String.format("File: %s Owner: %s Permission: %s", rs.getString("filename"),
+                            rs.getString("owner"), rs.getInt("permission") == 1 ? "RW" : "R"));
 
-            stmt = conn.prepareStatement("SELECT filename,owner,R,W FROM authorizations WHERE user = (?)");
-            stmt.setString(1, username);
-            rs = stmt.executeQuery();
-            while (rs.next()) {
-                result.add(rs.getString("filename") + " " + rs.getString("owner") +
-                        " R:" +  rs.getString("R")  + " W:" + rs.getString("W"));
+                }
             }
-
             return result;
 
         } catch (SQLException e) {
             throw new IllegalArgumentException("User has no files");
         }
-
     }
-
-
 }
