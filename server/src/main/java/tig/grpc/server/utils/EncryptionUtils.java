0363fe0cf19ddfc5070572d9592347630b2535ac
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2019-11-06 19:18:34 +0000

    File Upload encrypt

--- server/src/main/java/tig/grpc/server/utils/EncryptionUtils.java
+++ server/src/main/java/tig/grpc/server/utils/EncryptionUtils.java
@@ -1,14 +1,14 @@
 package tig.grpc.server.utils;
 
-import javax.crypto.*;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-import java.rmi.RemoteException;
-import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 public class EncryptionUtils {
 
-    public SecretKey generateAESKey() {
+    public static SecretKey generateAESKey() {
         try {
             KeyGenerator keyGen = KeyGenerator.getInstance("AES");
             keyGen.init(256); // for example
@@ -20,15 +20,27 @@ public class EncryptionUtils {
         }
     }
 
-    public SecretKeySpec getAesKey(byte[] encoding) {
+    public static SecretKeySpec getAesKey(byte[] encoding) {
         return new SecretKeySpec(encoding, 0, 16, "AES");
     }
 
-    public byte[] encryptFile(byte[] originalFile, SecretKey key) {
+    public static byte[] encryptFile(byte[] originalFile, SecretKey key) {
         try {
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
             cipher.init(Cipher.ENCRYPT_MODE, key);
             return cipher.doFinal(originalFile);
+        } catch (Exception e) {
+            //Should never happen
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public static byte[] decryptFile(byte[] encryptedFile, SecretKeySpec key) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            return cipher.doFinal(encryptedFile);
         } catch (Exception e) {
             //Should never happen
             e.printStackTrace();
