b0a44dbfb8ee3068060a5b8c34698dd3b673a1cf
daniela-mendes <daniela.mendes@tecnico.ulisboa.pt>
2019-11-14 12:28:56 +0000

    changed recoverFile

--- backup/src/main/java/tig/grpc/backup/api/BackupServerImpl.java
+++ backup/src/main/java/tig/grpc/backup/api/BackupServerImpl.java
@@ -1,10 +1,14 @@
 package tig.grpc.backup.api;
 
+import com.google.protobuf.ByteString;
 import io.grpc.stub.StreamObserver;
 import org.apache.log4j.Logger;
+import tig.grpc.backup.dao.FileDAO;
 import tig.grpc.contract.Tig;
 import tig.grpc.contract.TigBackupServiceGrpc;
 import com.google.protobuf.Empty;
+
+import java.util.Arrays;
 
 public class BackupServerImpl extends TigBackupServiceGrpc.TigBackupServiceImplBase {
     private final static Logger logger = Logger.getLogger(BackupServerImpl.class);
@@ -16,7 +20,23 @@ public class BackupServerImpl extends TigBackupServiceGrpc.TigBackupServiceImplB
 
     @Override
     public void recoverFile (Tig.RecoverFileRequest request, StreamObserver<Tig.FileChunkDownload> reply) {
+        logger.info(String.format("Recover file: %s", request.getFilename()));
 
+        String filename = request.getFilename();
+        String time_created = request.getTCreated();
+
+        byte[] file = FileDAO.getFileContent(filename, time_created);
+
+        int sequence = 0;
+        //Send file 1MB chunk at a time
+        for (int i = 0; i < file.length; i += 1024 * 1024, sequence++) {
+            int chunkSize = Math.min(1024 * 1024, file.length - i);
+            Tig.FileChunkDownload.Builder builder = Tig.FileChunkDownload.newBuilder();
+            builder.setContent(ByteString.copyFrom(Arrays.copyOfRange(file, i, i + chunkSize)));
+            builder.setSequence(sequence);
+            reply.onNext(builder.build());
+        }
+        reply.onCompleted();
     }
 
     @Override
--- backup/src/main/java/tig/grpc/backup/dao/FileDAO.java
+++ backup/src/main/java/tig/grpc/backup/dao/FileDAO.java
@@ -1,4 +1,34 @@
 package tig.grpc.backup.dao;
 
+import tig.utils.db.PostgreSQLJDBC;
+import tig.utils.encryption.EncryptedFile;
+import tig.utils.encryption.EncryptionUtils;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class FileDAO {
+
+    public static byte[] getFileContent(String filename, String t_created) {
+        Connection conn = PostgreSQLJDBC.getInstance().getConn();
+        try {
+            PreparedStatement stmt = conn.prepareStatement("SELECT content FROM files " +
+                    "WHERE filename = (?) and t_created = (?)");
+
+            stmt.setString(1, filename);
+            stmt.setString(2, t_created);
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+
+            return rs.getBytes("content");
+
+        } catch (SQLException e) {
+            //Should never happen
+            throw new RuntimeException();
+        }
+    }
+
 }
