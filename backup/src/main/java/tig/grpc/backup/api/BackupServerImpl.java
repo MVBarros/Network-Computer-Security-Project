de019e15cd51d6246233ef9a658641aaddec5e98
daniela-mendes <daniela.mendes@tecnico.ulisboa.pt>
2019-11-14 12:08:45 +0000

    added methods to BackupServerImpl

--- backup/src/main/java/tig/grpc/backup/api/BackupServerImpl.java
+++ backup/src/main/java/tig/grpc/backup/api/BackupServerImpl.java
@@ -4,8 +4,24 @@ import io.grpc.stub.StreamObserver;
 import org.apache.log4j.Logger;
 import tig.grpc.contract.Tig;
 import tig.grpc.contract.TigBackupServiceGrpc;
+import com.google.protobuf.Empty;
 
 public class BackupServerImpl extends TigBackupServiceGrpc.TigBackupServiceImplBase {
     private final static Logger logger = Logger.getLogger(BackupServerImpl.class);
+
+    @Override
+    public void listBackupFiles (Tig.ListBackupFilesRequest request, StreamObserver<Tig.ListFilesReply> reply) {
+
+    }
+
+    @Override
+    public void recoverFile (Tig.RecoverFileRequest request, StreamObserver<Tig.FileChunkDownload> reply) {
+
+    }
+
+    @Override
+    public StreamObserver<Tig.BackupFileUpload> insertFileBackup (StreamObserver<Empty> reply) {
+        return null;
+    }
 
 }
