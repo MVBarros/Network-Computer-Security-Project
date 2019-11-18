c3a528a7291773703e0f9a74e63a15ec95f92e64
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2019-11-18 23:03:56 +0000

    root-gen

--- keys/root-gen.sh
+++ keys/root-gen.sh
@@ -1,29 +1,34 @@
 #cleanup
-rm ca.key ca.cert server.key server.pem
+rm -rf root-ca
+mkdir root-ca
+mkdir root-ca/ca
+mkdir root-ca/server
 
 #generate ca key
-openssl genrsa -out root-ca/ca.key 4096
+openssl genrsa -out root-ca/ca/ca.key 4096
 
 #convert to Java format
-openssl pkcs8 -topk8 -in root-ca/ca.key -nocrypt -out root-ca/ca2.key
-mv root-ca/ca2.key root-ca/ca.key
+openssl pkcs8 -topk8 -in root-ca/ca/ca.key -nocrypt -out root-ca/ca/ca2.key
+mv root-ca/ca/ca2.key root-ca/ca/ca.key
 
 #self sign certificate
-openssl req -new -x509 -key root-ca/ca.key -sha256 -subj "/C=PT/ST=LX/O=TIG" -days 365 -out root-ca/ca.cert
+openssl req -new -x509 -key root-ca/ca/ca.key -sha256 -subj "/C=PT/ST=LX/O=TIG" -days 365 -out root-ca/ca/ca.cert
 
 #generate server key
-openssl genrsa -out root-ca/server.key
+openssl genrsa -out root-ca/server/server.key
 
 #convert to Java format
-openssl pkcs8 -topk8 -in root-ca/server.key -nocrypt -out root-ca/server2.key
-mv root-ca/server2.key root-ca/server.key
+openssl pkcs8 -topk8 -in root-ca/server/server.key -nocrypt -out root-ca/server/server2.key
+mv root-ca/server/server2.key root-ca/server/server.key
 
 #generate server signing request
-openssl req -new -key root-ca/server.key -out root-ca/server.csr -config certificate.conf
+openssl req -new -key root-ca/server/server.key -out root-ca/server/server.csr -config certificate.conf
 
 #sign server key with ca key
-openssl x509 -req -in root-ca/server.csr -CA root-ca/ca.cert -CAkey root-ca/ca.key -CAcreateserial -out root-ca/server.pem -days 365 -sha256 -extfile certificate.conf -extensions req_ext
+openssl x509 -req -in root-ca/server/server.csr -CA root-ca/ca/ca.cert -CAkey root-ca/ca/ca.key -CAcreateserial -out root-ca/server/server.pem -days 365 -sha256 -extfile certificate.conf -extensions req_ext
 
-#convert Server Key to Java format
-openssl pkcs8 -topk8 -inform PEM -outform DER -in root-ca/server.key -nocrypt -out root-ca/server_pcks8.key
+#convert Server Key to Java format (diferent from grpc format)
+openssl pkcs8 -topk8 -inform PEM -outform DER -in root-ca/server/server.key -nocrypt -out root-ca/server/server_pcks8.key
 
+#delete csr
+rm root-ca/server/server.csr
