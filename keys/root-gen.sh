3dc928ac3be120eb7d9527c2012ca3b37ea20401
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2019-11-24 13:07:09 +0000

    clients certs

--- keys/root-gen.sh
+++ keys/root-gen.sh
@@ -3,6 +3,7 @@ rm -rf root-ca
 mkdir root-ca
 mkdir root-ca/ca
 mkdir root-ca/server
+mkdir root-ca/client
 
 #generate ca key
 openssl genrsa -out root-ca/ca/ca.key 4096
@@ -30,5 +31,20 @@ openssl x509 -req -in root-ca/server/server.csr -CA root-ca/ca/ca.cert -CAkey ro
 #convert server private key to java format (diferent from grpc format)
 openssl pkcs8 -topk8 -inform PEM -outform DER -in root-ca/server/server.key -nocrypt -out root-ca/server/server_pcks8.key
 
+
+#generate client key
+openssl genrsa -out root-ca/client/client.key
+
+#generate client signing request
+openssl req -new -key root-ca/client/client.key -out root-ca/client/client.csr -config certificate.conf
+
+#sign client key with ca key
+openssl x509 -req -in root-ca/client/client.csr -CA root-ca/ca/ca.cert -CAkey root-ca/ca/ca.key -CAcreateserial -out root-ca/client/client.pem -days 365 -sha256 -extfile certificate.conf -extensions req_ext
+
+#convert client private key to java format
+openssl pkcs8 -topk8 -inform PEM -outform DER -in root-ca/client/client.key -nocrypt -out root-ca/client/client_pkcs8.key
+mv root-ca/client/client_pkcs8.key root-ca/client/client.key
+
 #delete csr
 rm root-ca/server/server.csr
+rm root-ca/client/client.csr
