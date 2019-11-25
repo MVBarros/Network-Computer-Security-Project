4850774a2ffe06568abdd4260b44fa69b21ccf1c
Carolina Carreira <carolina.carreira@tecnico.ulisboa.pt>
2019-11-25 15:31:53 +0000

    added server keys

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -2,17 +2,13 @@ drop table if exists authorizations;
 drop table if exists files;
 drop table if exists users;
 
-create table users(username varchar  primary key not null,
-				   passwordhash bytea not null, 
-				   passwordsalt bytea not null);
+create table users(username varchar  primary key not null);
 				   
 
 create table files(filename varchar  not null,
 				   fileowner varchar not null, 
 				   t_created varchar not null, 
 				   content bytea not null,
-				   encryption_key bytea not null,
-				   iv bytea not null,
 				  primary key (filename, fileowner),
 				  foreign key(fileowner) references users(username) on delete cascade) ;
 
