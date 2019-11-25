8054ee587794a8d53a8ab8f9f0fc76ae45bddca0
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2019-11-25 16:55:06 +0000

    Custom Protocol Logout

--- client/src/main/java/tig/grpc/client/operations/CustomProtocolOperations.java
+++ client/src/main/java/tig/grpc/client/operations/CustomProtocolOperations.java
@@ -137,8 +137,6 @@ public class CustomProtocolOperations {
 
 }
 
-
-
     /*public static void deleteFile(Client client, String filename) {
         try {
             System.out.println(String.format("Delete File %s ", filename));
