80c7562a0bc9241cf2b15d6149c438928882c0b3
Carolina Carreira <carolina.carreira@tecnico.ulisboa.pt>
2019-11-28 16:40:38 +0000

    added functionality to tigserverkey

---
+++ key-server/src/main/java/tig/grpc/keys/dao/UsersDAO.java
@@ -0,0 +1,59 @@
+package tig.grpc.keys.dao;
+
+import tig.utils.PasswordUtils;
+import tig.utils.db.PostgreSQLJDBC;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UsersDAO {
+
+    public static void insertUser(String username, String password) {
+        Connection conn = PostgreSQLJDBC.getInstance().getConn();
+        try {
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO users VALUES(?,?,?)");
+            stmt.setString(1, username);
+            byte[] salt = PasswordUtils.generateRandomSalt();
+            byte[] hash = PasswordUtils.generateStrongPasswordHash(password, salt);
+            stmt.setBytes(2, hash);
+            stmt.setBytes(3, salt);
+            int result = stmt.executeUpdate();
+            if (result == 0) {
+                //Should never happen
+                throw new RuntimeException();
+            }
+        } catch (SQLException e) {
+            //Username already exists (Will happen because of a PrimaryKeyViolation)
+            throw new IllegalArgumentException("Username already in use");
+        }
+    }
+
+    public static void authenticateUser(String username, String password) {
+        Connection conn = PostgreSQLJDBC.getInstance().getConn();
+        try {
+            PreparedStatement stmt = conn.prepareStatement("SELECT passwordhash, passwordsalt FROM users WHERE username =(?)");
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+            //Get single result
+            if (rs.next()) {
+                byte[] realHash = rs.getBytes("passwordhash");
+                byte[] salt = rs.getBytes("passwordsalt");
+                byte[] calculatedHash = PasswordUtils.generateStrongPasswordHash(password, salt);
+
+                if (!PasswordUtils.validatePassword(realHash, calculatedHash)) {
+                    throw new IllegalArgumentException("Invalid Password or Username");
+                }
+            } else {
+                //Query was empty
+                throw new IllegalArgumentException("No such Username");
+            }
+
+        } catch (SQLException e) {
+            //Should never happen
+            throw new RuntimeException();
+        }
+    }
+}
+
