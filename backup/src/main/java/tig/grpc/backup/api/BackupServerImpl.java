3e3dbd5a2ac180607082706c5ed96ba7a1b36269
Carolina Carreira <carolina.carreira@tecnico.ulisboa.pt>
2019-11-29 16:32:19 +0000

    added listRecoverFiles finished

--- backup/src/main/java/tig/grpc/backup/api/BackupServerImpl.java
+++ backup/src/main/java/tig/grpc/backup/api/BackupServerImpl.java
@@ -2,16 +2,24 @@ package tig.grpc.backup.api;
 
 import io.grpc.stub.StreamObserver;
 import org.apache.log4j.Logger;
+import tig.grpc.backup.dao.FileDAO;
 import tig.grpc.contract.Tig;
 import tig.grpc.contract.TigBackupServiceGrpc;
 import com.google.protobuf.Empty;
+
+import java.util.List;
 
 public class BackupServerImpl extends TigBackupServiceGrpc.TigBackupServiceImplBase {
     private final static Logger logger = Logger.getLogger(BackupServerImpl.class);
 
     @Override
     public void listBackupFiles (Tig.ListBackupFilesRequest request, StreamObserver<Tig.ListFilesReply> reply) {
-
+        logger.info("List files that can be recovered " + request.getFileowner());
+        List<String> files = FileDAO.listFiles(request.getFileowner());
+        Tig.ListFilesReply.Builder builder = Tig.ListFilesReply.newBuilder();
+        builder.addAllFileInfo(files);
+        reply.onNext(builder.build());
+        reply.onCompleted();
     }
 
     @Override
--- backup/src/main/java/tig/grpc/backup/dao/FileDAO.java
+++ backup/src/main/java/tig/grpc/backup/dao/FileDAO.java
@@ -1,4 +1,36 @@
 package tig.grpc.backup.dao;
 
+import tig.utils.db.PostgreSQLJDBC;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileDAO {
+
+    public static List<String> listFiles(String username) {
+        Connection conn = PostgreSQLJDBC.getInstance().getConn();
+        PreparedStatement stmt = null;
+        try {
+            stmt = conn.prepareStatement("SELECT  filename, fileowner,t_created FROM files WHERE fileowner = (?)");
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+            List<String> result = new ArrayList<>();
+            while (rs.next()) {
+                result.add(String.format("File:%s\tOwner:%s\tDate Created:%s", rs.getString("filename"), rs.getString("fileowner"), rs.getString("t_created")));
+            }
+            if (result.size() == 0) {
+                result.add("User has no files");
+            }
+            return result;
+        } catch (SQLException e) {
+            //Should never happen
+            throw new RuntimeException();
+        }
+    }
+
+
 }
