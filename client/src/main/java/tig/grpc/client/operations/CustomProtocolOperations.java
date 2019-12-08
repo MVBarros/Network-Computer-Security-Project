5a893799c6df1129ae08146fcf93d2031df8d565
Carolina Carreira <carolina.carreira@tecnico.ulisboa.pt>
2019-12-08 18:03:38 +0000

    fixe

--- client/src/main/java/tig/grpc/client/operations/CustomProtocolOperations.java
+++ client/src/main/java/tig/grpc/client/operations/CustomProtocolOperations.java
@@ -139,8 +139,6 @@ public class CustomProtocolOperations {
         }
     }
 
-
-
     public static void deleteFile(Client client, String filename) {
         try {
             System.out.println(String.format("Delete File %s ", filename));
