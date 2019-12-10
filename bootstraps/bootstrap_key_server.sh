e82e8602be8bc05407f29695fac57501f4dc9771
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2019-12-10 17:15:11 +0000

    update bootstrap

--- bootstraps/bootstrap_key_server.sh
+++ bootstraps/bootstrap_key_server.sh
@@ -2,8 +2,6 @@ apt update && apt upgrade
 apt install sqlite3 -y
 apt install openjdk-8-jdk openjdk-8-jre -y
 sudo update-java-alternatives --set java-1.8.0-openjdk-amd64
-apt install maven -y 
-cd /home/vagrant/proj
-mvn clean install
+apt install maven -y
 cp -f /home/vagrant/proj/interfaces/interface_key_server.yaml /etc/netplan/60-gateway.yaml
 netplan apply
--- bootstraps/bootstrap_server.sh
+++ bootstraps/bootstrap_server.sh
@@ -3,7 +3,5 @@ apt install sqlite3 -y
 apt install openjdk-8-jdk openjdk-8-jre -y
 sudo update-java-alternatives --set java-1.8.0-openjdk-amd64
 apt install maven -y 
-cd /home/vagrant/proj
-mvn clean install
 cp -f /home/vagrant/proj/interfaces/interface_server.yaml /etc/netplan/60-gateway.yaml
 netplan apply
