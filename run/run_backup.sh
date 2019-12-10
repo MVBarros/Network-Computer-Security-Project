4d44f54a9563635d8203e581c7d02a7c5981d0db
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2019-12-10 11:19:51 +0000

    scripts to launch server

---
+++ run/run_backup.sh
@@ -0,0 +1,3 @@
+java -jar ../backup/target/tig-backup-1.0.0.RELEASE-jar-with-dependencies.jar 8091 ../backup/src/main/resources/db/backupbd \
+../backup/src/main/resources/certs/backup-server.pem ../backup/src/main/resources/certs/backup-server.key \
+../backup/src/main/resources/certs/ca.cert 192.168.50.10:8095
---
+++ run/run_keys.sh
@@ -0,0 +1,3 @@
+java -jar ../key-server/target/tig-keys-1.0.0-RELEASE-jar-with-dependencies.jar 8090 ../key-server/src/main/resources/db/tigkeyserver \
+../key-server/src/main/resources/certs/key-server.pem ../key-server/src/main/resources/certs/key-server.key \
+../key-server/src/main/resources/certs/ca.cert 8095
