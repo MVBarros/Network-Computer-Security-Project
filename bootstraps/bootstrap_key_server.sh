eb750bf464a826f3abcc7c4f0f3fce1b185e0761
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2019-12-10 01:31:45 +0000

    Possible fix

--- bootstraps/bootstrap_key_server.sh
+++ bootstraps/bootstrap_key_server.sh
@@ -1,5 +1,10 @@
 apt update && apt upgrade
 apt install sqlite3 -y
+apt install openjdk-8-jdk openjdk-8-jre -y
+sudo update-java-alternatives --set java-1.8.0-openjdk-amd64
 apt install maven -y 
+cd /home/vagrant/proj
+mvn -o clean install
+mvn dependency:go-offline
 cp -f /home/vagrant/proj/interfaces/interface_key_server.yaml /etc/netplan/60-gateway.yaml
 netplan apply
